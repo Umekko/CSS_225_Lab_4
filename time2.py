79a12c5d8bedeb63215d11ded9256906e8acd7a1
Chyngyz Mirzamatov <37297925+Umekko@users.noreply.github.com>
2025-05-04 15:20:37 -0500

    Update time2.py
    Updated comments

--- time2.py
+++ time2.py
@@ -1,3 +1,15 @@
+"""
+Calculating wait time 
+===============================
+
+Author: Chyngyz Mirzamatov
+Created on: Sunday, May 5 2025
+Last updated on: Sunday, May 5 2025
+
+Program take as input current time and wait time, convert to integer, calculate final time after wait
+"""
+
+
 str_time = input("What time is it now? ")
 str_wait_time = input("What is the number of hours to wait? ")
 time = int(str_time)
