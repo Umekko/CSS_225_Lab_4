304be42294206636fa8c4b9fac74b943539e82fe
Chyngyz Mirzamatov <37297925+Umekko@users.noreply.github.com>
2025-05-04 15:18:44 -0500

    Update pirate.py
    Updated comments

--- pirate.py
+++ pirate.py
@@ -1,4 +1,13 @@
+"""
+Detect pirate !
+===============================
 
+Author: Chyngyz Mirzamatov
+Created on: Sunday, May 5 2025
+Last updated on: Sunday, May 5 2025
+
+Program that  ask user for password. If it's pirate go away if now greet him
+"""
 greeting = input("Hello, possible pirate! What's the password?")
                       
 if greeting in ("Arrr!"):
